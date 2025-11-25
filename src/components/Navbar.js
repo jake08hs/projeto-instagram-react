2b42b1a6f73054a0af5561cf9ad968313a60b72b
jake08hs <jake08hs@gmail.com>
2025-11-25 15:50:00 -0300

    Refatora layout da Navbar e Stories: ajusta espaçamentos, alinha ícones e melhora a apresentação visual

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,28 @@
-export default function Navbar() { 
-    return (
-        <div className="navbar">
-            <div className="container">
+import React from "react";
 
-                {/* Logo como ícone */}
-                <div className="logo">
-                    <ion-icon name="logo-instagram"></ion-icon>
-                </div>
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="container">
 
-                <div className="pesquisa">
-                    <input placeholder="Pesquisar" />
-                </div>
-  
-                <div className="icones">
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                    <ion-icon name="compass-outline"></ion-icon>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="person-outline"></ion-icon>
-                </div>
-            </div>
+       
+        <div className="logo">
+          <ion-icon name="logo-instagram"></ion-icon>
+        <img src="/assets/img/logo.png" className="nav-logo" alt="Instagram Logo" />
+
         </div>
-    );
+
+        <div className="pesquisa">
+          <input placeholder="Pesquisar" />
+        </div>
+
+        <div className="icones">
+          <ion-icon name="paper-plane-outline"></ion-icon>
+          <ion-icon name="compass-outline"></ion-icon>
+          <ion-icon name="heart-outline"></ion-icon>
+          <ion-icon name="person-outline"></ion-icon>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,3 +1,31 @@
 export default function Stories() {
-    return <div className="stories"></div>;
+    const stories = [
+        { img: "assets/img/9gag.svg", user: "9gag" },
+        { img: "assets/img/meowed.svg", user: "meowed" },
+        { img: "assets/img/barked.svg", user: "barked" },
+        { img: "assets/img/nathanwpylestrangeplanet.svg", user: "nathanwpylestrangeplanet" },
+        { img: "assets/img/wawawicomics.svg", user: "wawawicomics" },
+        { img: "assets/img/respondeai.svg", user: "respondeai" },
+        { img: "assets/img/filomoderna.svg", user: "filomoderna" },
+        { img: "assets/img/memeriagourmet.svg", user: "memeriagourmet" }
+    ];
+
+    return (
+        <div className="stories-container">
+            <div className="stories-scroll">
+                {stories.map((s) => (
+                    <div className="story" key={s.user}>
+                        <div className="story-border">
+                            <img src={s.img} alt={s.user} />
+                        </div>
+                        <div className="story-user">{s.user}</div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="stories-arrow">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+        </div>
+    );
 }
